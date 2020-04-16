017ee034aad0d8148ceb08ac4298908ae02a185c
bot <lehilton_bot@ic.unicamp.br>
2020-04-15 23:43:29 -0300

    Adicionada tarefa05

---
+++ tarefa05/testes/triangulo6.res
@@ -0,0 +1,1 @@
+obstusângulo
---
+++ tarefa05/triangulo.py
@@ -0,0 +1,9 @@
+# lê uma string com três partes
+a, b, c = input().split()
+
+# converte strings em números
+a = float(a)
+b = float(b)
+c = float(c)
+
+# termine esse programa aqui...
