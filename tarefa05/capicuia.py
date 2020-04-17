17698a2e11fdfa5d23aa7f7067cc6c50130e1053
Vitor Hiratsuka <v256589@dac.unicamp.br>
2020-04-17 18:02:13 -0300

    tarefa05

---
+++ tarefa05/capicuia.py
@@ -0,0 +1,20 @@
+﻿numero= int(input());
+numeroString=str(numero);
+if len(numeroString)<=4:
+    if len(numeroString)==1:
+        print("sim")
+    elif len(numeroString)==2:
+        if numeroString[0]==numeroString[1]:
+            print("sim")
+        else:
+            print("não")
+    elif len(numeroString)==3:
+        if numeroString[0]==numeroString[2]:
+            print("sim")
+        else:
+            print("não")
+    elif len(numeroString)==4:
+        if numeroString[0]==numeroString[3] and numeroString[1]==numeroString[2]:
+            print("sim")
+        else:
+            print("não")
--- tarefa05/triangulo.py
+++ tarefa05/triangulo.py
@@ -1,9 +1,30 @@
-# lê uma string com três partes
+﻿# lê uma string com três partes
 a, b, c = input().split()
 
 # converte strings em números
 a = float(a)
 b = float(b)
 c = float(c)
-
+numeroMaior=0
 # termine esse programa aqui...
+if a+b>c and a+c>b and b+c>a:
+    numeroMaior= a;
+    if(numeroMaior<b):
+        numeroMaior=b;
+    if(numeroMaior<c):
+        numeroMaior=c;
+    if(numeroMaior==a):
+        cosseno= -(numeroMaior**2 - (b**2 + c**2))/(2*b*c)
+    elif(numeroMaior==b):
+        cosseno= -(numeroMaior**2 - (a**2 + c**2))/(2*a*c)
+    elif(numeroMaior==c):
+        cosseno= -(numeroMaior**2 - (a**2 + b**2))/(2*a*b)
+    if(cosseno==0):
+        print("retângulo")
+    elif(cosseno>0):
+        print("acutângulo")
+    elif(cosseno<0):
+        print("obtusângulo")
+else:
+    print("não forma triângulo");
+
