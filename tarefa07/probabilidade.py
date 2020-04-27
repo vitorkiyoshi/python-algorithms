988e0848deffa4c58ef20103798fe4efa4c3c116
Vitor Hiratsuka <v256589@dac.unicamp.br>
2020-04-27 17:51:20 -0300

    probabilidade modificada

--- tarefa07/probabilidade.py
+++ tarefa07/probabilidade.py
@@ -1,33 +1,35 @@
+ListaP=[]
+seqProb=[]
+seqNum=[]
 def contar(x):
     for i in range(0,len(x)):
-        for j in range(0,len(x)):
-            if x[i]==x[j] and x[i] not in seqNum:
-                seqNum.append(x[i])
+        if x[i] not in seqNum:
+            seqNum.append(x[i])
 def calcularProb(y):
     prob=0
     for j in range(0,len(ListaP)):
         if y==ListaP[j]:
            prob = prob + 1
     seqProb.append(prob)
-ListaP=[]
-seqProb=[]
-seqNum=[]
-L= input().split(' ')
-for i in range(0,len(L)):
-    ListaP.append(int(L[i]))    #transforma em inteiros
-contar(ListaP)  #separa cada numero para contagem
-for i in range(0,len(seqNum)):
-    calcularProb(seqNum[i])    #calcula quantas vezes apareceu tal numero
-for i in range(0,len(seqNum)):      #ordenando em ordem de prob
-    for j in range(0,len(seqNum)):
-        if int(seqProb[j])>int(seqProb[i]):
-            seqNum[j],seqNum[i]= seqNum[i],seqNum[j]
-            seqProb[j],seqProb[i]= seqProb[i],seqProb[j]
-for i in range(0,len(seqNum)):      #ordenando em ordem crescente os termos separados
-    for j in range(0,len(seqNum)):
-        if(j>i): #apenas trocando com os próximos números da lista
-            if int(seqNum[i])>int(seqNum[j]):
+def ordenar():
+    for i in range(0,len(seqNum)):      #ordenando em ordem de prob
+        for j in range(i+1,len(seqNum)):
+            if int(seqNum[i])>int(seqNum[j]):#ordenação em crescente com condição da probabilidade
                 if not seqProb[j]>seqProb[i]:
                     seqNum[j],seqNum[i]= seqNum[i],seqNum[j]
                     seqProb[j],seqProb[i]= seqProb[i],seqProb[j]
-print(*seqNum)
+            if int(seqProb[i])>int(seqProb[j]): #ordenação de probabilidade
+                seqNum[i],seqNum[j]= seqNum[j],seqNum[i]
+                seqProb[i],seqProb[j]= seqProb[j],seqProb[i]
+def main():
+    L= input().split(' ')
+    for i in range(0,len(L)):
+        ListaP.append(int(L[i]))    #transforma em inteiros
+    contar(ListaP)  #separa cada numero para contagem
+    for i in range(0,len(seqNum)):
+        calcularProb(seqNum[i])    #calcula quantas vezes apareceu tal numero
+    ordenar()
+    print(*seqNum)
+main()
+
+
